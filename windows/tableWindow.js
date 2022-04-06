11d63ff2b93ec2ec4af4fd03d8f7cb7398789061
Habibul Islam <habibulislam6862@gmail.com>
2022-04-06 18:06:18 +0600

    insert table window fixed

--- windows/tableWindow.js
+++ windows/tableWindow.js
@@ -10,16 +10,22 @@ const tableWindow = (parent) => {
     modal: true,
     show: false,
     frame: false,
+    resizable: false,
     parent,
     webPreferences: {
       preload: path.join(__dirname, '..', 'preloads', 'tablePreloads.js'),
     },
   });
-  // show table after click "Insert table" item from menu
+
+  // hide table after cliking on cancle button from modal
+  ipcMain.handle('cancel-insert', () => {
+    table.hide();
+  });
+  // show table after clicking on "Insert table" item from menu
   ipcMain.handle('show-table', () => {
     table.show();
   });
-  ipcMain.handle('test', () => {
+  ipcMain.handle('close', () => {
     table.close();
   });
   table.loadFile(path.join(__dirname, '..', 'src', 'windows', 'insert-table.html'));
